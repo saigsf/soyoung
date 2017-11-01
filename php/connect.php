7b0e80742c8b0d03c06279c1173587c8b9dad0c5
saigsf <2723799947@qq.com>
2017-11-01 21:12:29 +0800

    数据

--- php/connect.php
+++ php/connect.php
@@ -1,12 +1,12 @@
 <?php
-// $mysqlhost="qdm114548101.my3w.com";
-// $mysqlname="qdm114548101";
-// $password="saigsf2270029397";
-// $dbname="qdm114548101_db";
-$mysqlhost="69.171.77.35";
-$mysqlname="root";
-$password="saigsf";
-$dbname="message";
+$mysqlhost="qdm114548101.my3w.com";
+$mysqlname="qdm114548101";
+$password="saigsf2270029397";
+$dbname="qdm114548101_db";
+// $mysqlhost="69.171.77.35";
+// $mysqlname="root";
+// $password="saigsf";
+// $dbname="message";
 $conn = mysql_connect($mysqlhost, $mysqlname, $password);
 mysql_select_db($dbname, $conn);
 //设置字符集
